43507ccc2d501ac1a7a8d2c29a5c376753b2a645
Renat Yakublevich <49581209+RenatYakublevich@users.noreply.github.com>
2021-01-31 03:43:36 +0300

    1.2
    add presets

--- main.py
+++ main.py
@@ -6,64 +6,93 @@ class _Request:
         self.url = url
         self.options = kwargs
 
-    def do_request(self, type_request, type_return):
+    def send_request(self, type_request, type_return):
         """
         :param type_request: тип HTTP запроса
-        :param type_return: тип возвращаемого значения(text / json)
-        :return: метод возвращает текст запроса
+        :param type_return: тип возвращаемого значения -> response_items
+        :return: метод возвращает ответ сервера на запрос
         """
         response = requests.request(type_request, self.url, **self.options)
 
         response_items = {
             'text' : response.text,
-            'json' : response.json,
             'status_code' : response.status_code
         }
         return response_items[type_return]
 
 class HTTPAlchemy:
-    @staticmethod
-    def menu():
-        menu_items = [
-            '1 - make a requests'
+    def __init__(self):
+        self.request_items = dict()
+
+    def menu(self):
+        menu_list = [
+            '1.Make a requests',
+            '2.Save Preset',
+            '3.Load Preset'
         ]
-        unpacking_menu_items = '\n'.join(menu_items)
+        unpacking_menu_items = '\n'.join(menu_list)
         menu_input = input(f"Hello!\n{unpacking_menu_items}\nWrite number: ")
 
-        menu = {
-            '1' : HTTPAlchemy.do_request
+        menu_items = {
+            '1': [self.input_info_request, self.do_request],
+            '2': [self.input_info_request, self.save_preset],
+            '3': [self.load_preset, self.do_request]
         }
         try:
-            menu[menu_input]()
+            [command() for command in menu_items[menu_input]]
         except KeyError:
             print('There is no such menu number!')
 
-    @staticmethod
-    def do_request():
-        while True:
-            parameters = input('Enter the request type and link separated by a space and the type of the returned request (example - get https://www.google.com/ json): ')
+    def do_request(self):
+        try:
+            type_requests = self.parameters.split(' ')[0]
+            url = self.parameters.split(' ')[1]
+            type_return = self.parameters.split(' ')[2]
+        except IndexError:
+            print('Wrong format!')
+        if self.request_items:
+            request = _Request(url, **self.request_items)
+        else:
+            request = _Request(url)
+        print(f'Server response:\n{request.send_request(type_requests, type_return)}')
 
-            try:
-                type_requests = parameters.split(' ')[0]
-                url = parameters.split(' ')[1]
-                type_return = parameters.split(' ')[2]
-            except IndexError:
-                print('Wrong format!')
+    def input_info_request(self):
+        self.parameters = input('Enter the request type and link separated by a space and the type of the returned request (example - get https://www.google.com/ json): ')
+        additional_arguments = input('Enter the parameters that you want to use when prompted, separated by a space(params, data, json, headers,none if you dont want to specify parameters): ')
+        self.request_items = {}
 
-            additional_arguments = input('Enter the parameters that you want to use when prompted, separated by a space(params, data, json, headers,none if you dont want to specify parameters): ')
-            request_items = {}
-
-            if additional_arguments != 'none':
+        if additional_arguments.lower() != 'none':
                 for argument in additional_arguments.split(' '):
                     arg = input(f'Enter the {argument} of request: ')
-                    request_items[argument] = eval(arg)
+                    self.request_items[argument] = eval(arg)
 
-            request = _Request(url, **request_items)
-            print(f'Server response:\n{request.do_request(type_requests, type_return)}')
+    def save_preset(self):
+        with open('presets.txt','r',encoding='utf-8') as file:
+            all_lines = [line.strip() for line in file]
+            self.count_presets = len(list(filter(lambda x: x.startswith('Preset #'), all_lines)))
+
+        with open('presets.txt','a',encoding='utf-8') as file:
+            file.write(f'\nPreset #{self.count_presets + 1}\n')
+            file.write('\n'.join([self.parameters]) + '\n')
+            keys = [key for key in self.request_items.keys()]
+            values = [value for value in self.request_items.values()]
+            for el in range(len(self.request_items.keys())):
+                file.write(f'{keys[el]}={values[el]} ')
+
+    def load_preset(self):
+        number_preset = int(input('Write number preset: '))
+        with open('presets.txt','r',encoding='utf-8') as file:
+            all_lines = [line.strip() for line in file]
+            count_line_info = all_lines.index(f'Preset #{number_preset}')
+            self.parameters = all_lines[count_line_info + 1]
+            try:
+                request_items_line = all_lines[count_line_info + 2]
+                for request_item in range(int(len(request_items_line.split('=')))):
+                    self.request_items[request_items_line.split('=')[request_item]] = eval(request_items_line.split('=')[request_item + 1]) 
+
+            except IndexError:
+                pass
 
 
-
-http_alchemy = HTTPAlchemy()
-http_alchemy.menu()
-
-
+alchemy = HTTPAlchemy()
+alchemy.menu()
